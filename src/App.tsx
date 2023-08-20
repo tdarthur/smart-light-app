361e24f0d798343769621b9e65c0826004fc7ec7
Tyler <tdarthur3@gmail.com>
2023-08-19 23:51:21 -0400

    implements landing with basic graphics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 function App() {
-    return <>hey</>;
+    return (
+        <div className="landing">
+            <div className="landing-graphic landing-spotlight" />
+            <div className="landing-graphic landing-pointlight" />
+
+            <div className="landing-text">
+                <h2>Let's make your life</h2>
+                <h1>Illuminous</h1>
+            </div>
+            <div className="landing-action-text">Check out our products below</div>
+        </div>
+    );
 }
 
 export default App;
