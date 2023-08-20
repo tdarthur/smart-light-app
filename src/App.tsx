ef2296d6e2da9e7a98f8b4888878eea7d9dc9454
Tyler <tdarthur3@gmail.com>
2023-08-20 00:38:31 -0400

    implements fading scroll effect for landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-function App() {
-    return (
-        <div className="landing">
-            <div className="landing-graphic landing-spotlight" />
-            <div className="landing-graphic landing-pointlight" />
+import useScrollPosition from './hooks/useScrollPosition';
 
-            <div className="landing-text">
-                <h2>Let's make your life</h2>
-                <h1>Illuminous</h1>
+function App() {
+    const scrollPosition = useScrollPosition();
+
+    console.log(scrollPosition);
+
+    return (
+        <>
+            <div className="landing" style={{ opacity: 1 - scrollPosition / window.innerHeight }}>
+                <div className="landing-graphic landing-spotlight" />
+                <div className="landing-graphic landing-pointlight" />
+
+                <div className="landing-text">
+                    <h2>Light up your life with</h2>
+                    <h1>Illuminous</h1>
+                </div>
+                <div className="landing-action-text">Check out our products below</div>
             </div>
-            <div className="landing-action-text">Check out our products below</div>
-        </div>
+
+            <div className="products"></div>
+
+            <div className="demo"></div>
+        </>
     );
 }
 
