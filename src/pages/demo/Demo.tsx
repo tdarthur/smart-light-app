fa08432e3e00321de495544293ef5deb08a84a62
Tyler <tdarthur3@gmail.com>
2023-08-23 23:58:14 -0400

    adds spline for demo

--- src/pages/demo/Demo.tsx
+++ src/pages/demo/Demo.tsx
@@ -1,3 +1,17 @@
-const Demo = () => <div className="demo"></div>;
+import { lazy } from 'react';
+import styles from './demo.module.css';
+import useScrollPosition from '../../hooks/useScrollPosition';
+
+const LazySpline = lazy(() => import('@splinetool/react-spline'));
+
+const Demo = () => {
+    const scrollPosition = useScrollPosition();
+
+    return (
+        <div className={styles.demo} style={{ opacity: scrollPosition / window.innerHeight }}>
+            <LazySpline scene="https://prod.spline.design/5ar9EaBHYxKDJ34t/scene.splinecode" />
+        </div>
+    );
+};
 
 export default Demo;
