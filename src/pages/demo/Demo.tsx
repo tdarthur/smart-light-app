fda1248d2565000dcd2656c4307af0198c41277c
tdarthur <tdarthur3@gmail.com>
2023-08-24 23:03:57 -0400

    makes elements more responsive

--- src/pages/demo/Demo.tsx
+++ src/pages/demo/Demo.tsx
@@ -1,17 +1,10 @@
-import { lazy } from 'react';
 import styles from './demo.module.css';
 import useScrollPosition from '../../hooks/useScrollPosition';
-
-const LazySpline = lazy(() => import('@splinetool/react-spline'));
 
 const Demo = () => {
     const scrollPosition = useScrollPosition();
 
-    return (
-        <div className={styles.demo} style={{ opacity: scrollPosition / window.innerHeight }}>
-            <LazySpline scene="https://prod.spline.design/5ar9EaBHYxKDJ34t/scene.splinecode" />
-        </div>
-    );
+    return <div className={styles.demo} style={{ opacity: scrollPosition / window.innerHeight }} />;
 };
 
 export default Demo;
