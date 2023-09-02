caf525350035ddcaab07870a9b75f96de9edb0ca
tdarthur <tdarthur3@gmail.com>
2023-09-01 22:59:45 -0400

    modifies landing fade

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import type { ComponentProps } from 'react';
+import clsx from 'clsx';
+
 import styles from './components.module.css';
 
-const Header = () => (
-    <header className={styles.header}>
+const Header = ({ className, ...props }: ComponentProps<'header'>) => (
+    <header className={clsx(styles.header, className)} {...props}>
         <div className={styles.headerLogo}>Illuminous</div>
     </header>
 );
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,12 +1,19 @@
+import clsx from 'clsx';
+
 import Header from '../../components/Header';
+
 import styles from './home.module.css';
+
+const fadeIn = !sessionStorage.getItem('visited');
+sessionStorage.setItem('visited', 'true');
 
 const Home = () => {
     return (
         <>
-            <Header />
-            <div className={styles.landingOverlay}>Light up your life</div>
-            <main className={styles.homePage}>
+            {fadeIn && <div className={styles.landingUnderlay}>Light up your life</div>}
+
+            <Header className={clsx(fadeIn && styles.landingFade)} />
+            <main className={clsx(styles.homePage, fadeIn && styles.landingFade)}>
                 <section className={styles.aboutSection}>
                     <h2>This is some placeholder text</h2>
                     <p>This is some other placeholder text</p>
