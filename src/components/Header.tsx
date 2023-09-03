43437089522294f5093039e863c36c3817da1f5c
tdarthur <tdarthur3@gmail.com>
2023-09-03 19:01:05 -0400

    adds light/dark mode toggle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,55 @@
-import type { ComponentProps } from "react";
+import { useState, type ComponentProps } from "react";
 import clsx from "clsx";
 
 import styles from "./components.module.css";
 import { useLocation, useNavigate } from "react-router-dom";
 
+let lightModeEnabled = localStorage.getItem("light-mode") === "true";
+if (lightModeEnabled) {
+	document.body.classList.add("light-mode");
+}
+
 const Header = ({ className, ...props }: ComponentProps<"header">) => {
+	const [lightMode, setLightMode] = useState(lightModeEnabled);
+
 	const location = useLocation();
 	const navigate = useNavigate();
 
 	return (
 		<>
 			<header className={clsx(styles.header, className)} {...props}>
+				<div className={styles.headerLogo}>
+					<button
+						className={styles.headerLogoText}
+						data-text="Illuminous"
+						onClick={() => {
+							if (location.pathname !== "/") {
+								navigate("/");
+							}
+						}}
+					>
+						Illuminous
+					</button>
+				</div>
+
 				<button
-					className={styles.headerLogo}
-					data-text="Illuminous"
+					className={styles.headerDarkModeToggle}
 					onClick={() => {
-						if (location.pathname !== "/") {
-							navigate("/");
+						lightModeEnabled = !lightModeEnabled;
+
+						if (lightModeEnabled) {
+							document.body.classList.add("light-mode");
+							localStorage.setItem("light-mode", "true");
+						} else {
+							document.body.classList.remove("light-mode");
+							localStorage.setItem("light-mode", "false");
 						}
+
+						setLightMode(lightModeEnabled);
 					}}
 				>
-					Illuminous
+					{lightMode ? "Light Mode" : "Dark Mode"}
 				</button>
-
-				<button />
 			</header>
 			<div className={styles.headerSpacer} />
 		</>
