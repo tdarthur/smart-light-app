d6f98714712e0cec72b92cf4345028d549e697bd
Tyler <tdarthur3@gmail.com>
2023-09-10 22:12:48 -0400

    adds bulb images and product list json

--- src/routes/Products/Products.tsx
+++ src/routes/Products/Products.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 import Header from "../../components/Header";
+
 import styles from "./products.module.css";
 
 type ProductInfo = {
@@ -10,45 +13,51 @@ type ProductInfo = {
 	price: number;
 };
 
-const products: ProductInfo[] = [
-	{
-		id: "smart-bulb-standard",
-		name: "Smart Bulb",
-		image: "https://www.vont.com/i/11907?w=1200",
-		features: ["800 lumen", "full color range vibrant LED", "supports Alexa, Google Home, and Homekit"],
-		price: 19.99,
-	},
-	{
-		id: "smart-bulb-flood",
-		name: "Smart Flood Bulb",
-		image: "https://www.vont.com/i/11907?w=1200",
-		features: ["1100 lumen", "full color range vibrant LED", "supports Alexa, Google Home, and Homekit"],
-		price: 24.99,
-	},
-];
+const Products = () => {
+	// const [searchString, setSearchString] = useState("");
+	const [products, setProducts] = useState<ProductInfo[]>([]);
 
-const Products = () => (
-	<>
-		<Header />
-		<main className={styles.products}>
-			<div className={styles.productList}>
-				{products.map(({ id, name, image, features, price }) => (
-					<Link className={styles.product} to={`${id}`} key={name}>
-						<img className={styles.productImage} src={image} key={name} />
-						<div className={styles.productPrice}>{price.toLocaleString("en-US")}</div>
-						<div className={styles.productDetails}>
-							<h3 className={styles.productName}>{name}</h3>
-							<ul className={styles.productFeatures}>
-								{features.map((feature) => (
-									<li key={feature}>{feature}</li>
-								))}
-							</ul>
-						</div>
-					</Link>
-				))}
-			</div>
-		</main>
-	</>
-);
+	useEffect(() => {
+		(async () => {
+			setProducts((await (await fetch("/products.json")).json()) as ProductInfo[]);
+		})();
+	}, []);
+
+	return (
+		<>
+			<Header />
+			<main className={styles.products}>
+				<div className={styles.productSection}>
+					{/* <div className={styles.productSearchBar}>
+						<input
+							name="product-search"
+							className={styles.productSearchInput}
+							onChange={(event) => {
+								setSearchString(event.target.value);
+								console.log(searchString);
+							}}
+						/>
+					</div> */}
+					<div className={styles.productList}>
+						{products.map(({ id, name, image, features, price }) => (
+							<Link className={styles.product} to={`${id}`} key={name}>
+								<img className={styles.productImage} src={image} key={name} />
+								<div className={styles.productPrice}>{price.toLocaleString("en-US")}</div>
+								<div className={styles.productDetails}>
+									<h3 className={styles.productName}>{name}</h3>
+									<ul className={styles.productFeatures}>
+										{features.map((feature) => (
+											<li key={feature}>{feature}</li>
+										))}
+									</ul>
+								</div>
+							</Link>
+						))}
+					</div>
+				</div>
+			</main>
+		</>
+	);
+};
 
 export default Products;
